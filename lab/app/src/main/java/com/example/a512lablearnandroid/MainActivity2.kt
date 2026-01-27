ecfea9bd7da953e3234473b4a99aab9801b7c231
RaiFew <92487832+RaiFew@users.noreply.github.com>
2026-01-27 15:50:08 +0700

    Input text func

--- lab/app/src/main/java/com/example/a512lablearnandroid/MainActivity2.kt
+++ lab/app/src/main/java/com/example/a512lablearnandroid/MainActivity2.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a512lablearnandroid.ui.theme._512LabLearnAndroidTheme
 
 class MainActivity2 : ComponentActivity() {
@@ -32,10 +44,21 @@ class MainActivity2 : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var inputText by remember { mutableStateOf( value = "") }
+
+    Column {
+        Text(
+            text = "Hello Mr/Mrs "+inputText+". ",
+            modifier = modifier
+        )
+        TextField(
+            value = inputText,
+            onValueChange = {
+                inputText = it
+            }
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
